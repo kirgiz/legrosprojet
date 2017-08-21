11a9610a87830a961800914162dfd8271adda690
Olivier Bayle <obayle@sage.ch>
2017-08-21 13:33:13 +0200

    add constants

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,4 +1,14 @@
+import { Client } from './client';
 import { Injectable } from '@angular/core';
+
+const CLIENT: Client[] = [
+  { id: 11, name: 'Client Mr. Nice', matchCode: 'Client NICE', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 12, name: 'Client Mr. Narco', matchCode: 'Client NARCO', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 13, name: 'Client Mr. Bombasto', matchCode: 
+  'Client BOMBASTO', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 14, name: 'Client Mr. Celeritas', matchCode: 
+  'Client CELERITAS', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null}
+];
 
 @Injectable()
 export class ClientService {
--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -1,4 +1,12 @@
+import { Portfolio } from './portfolio';
 import { Injectable } from '@angular/core';
+
+const PORTFOLIOS: Portfolio[] = [
+  { id: 11, name: 'Mr. Nice', matchCode: 'NICE', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 12, name: 'Mr. Narco', matchCode: 'NARCO', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 13, name: 'Mr. Bombasto', matchCode: 'BOMBASTO', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null},
+  { id: 14, name: 'Mr. Celeritas', matchCode: 'CELERITAS', dateOpen: new Date(Date.UTC(2013, 1, 1, 14, 0, 0)) , dateClose: null}
+];
 
 @Injectable()
 export class PortfolioService {

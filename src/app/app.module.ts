3bbb4a666a69275a029a3e25d8f52a3771ae3b13
Olivier Bayle <obayle@sage.ch>
2017-08-21 14:18:22 +0200

    create pfo and client details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ClientComponent } from './client/client.component';
+import { ClientDetailComponent } from './client/client-detail/client-detail.component';
+import { PortfolioDetailComponent } from './portfolio/portfolio-detail/portfolio-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    ClientComponent
+    ClientComponent,
+    ClientDetailComponent,
+    PortfolioDetailComponent
   ],
   imports: [
     BrowserModule

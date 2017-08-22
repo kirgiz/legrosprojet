640be0ce3aa0597c8a0dd44c73171a2fe6373f2e
Olivier Bayle <obayle@sage.ch>
2017-08-22 19:40:06 +0200

    pouet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { PortfolioService } from './portfolio.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ClientComponent } from './client/client.component';
 import { ClientDetailComponent } from './client/client-detail/client-detail.component';
 import { PortfolioDetailComponent } from './portfolio/portfolio-detail/portfolio-detail.component';
 import { MdButtonModule, MdMenuModule, MdCardModule, MdToolbarModule,
-   MdIconModule, MdInputModule, MdPaginator} from '@angular/material';
+   MdIconModule, MdInputModule, MdPaginator } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
